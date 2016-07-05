912d280774dbc423bc18c4cc868307e63c626f28
Adrian <adrian@adrians-mac.local>
2016-07-05 04:29:44 +0200

    Initial Commit

---
+++ Gymoji/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gymoji
+//
+//  Created by Adrian on 7/5/16.
+//  Copyright © 2016 bodytech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
